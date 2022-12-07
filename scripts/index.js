129d1212a6e9d67cc9fd0cadb91b761546d4afde
David Inyangetoh <dinyangetoh@gmail.com>
2022-12-07 13:59:57 +0100

    add support for initiating transaction

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,27 @@
 console.log("External Script Loaded")
-const { ethereum } = window;
+const {ethereum} = window;
 const connectButton = document.getElementById('connectButton')
 const activeAccountId = document.getElementById('activeAccount')
-let accounts=[];
+const sendTransactionButton = document.getElementById('sendTransaction')
+let accounts = [];
 let activeAccount;
 
-const initialize = async ()=> {
+const initialize = async () => {
     initializeOnboarding();
-    if(ethereum.isConnected && (accounts.length===0 || accounts.length>0 && activeAccount !== accounts[0])){
+    if (ethereum.isConnected && (accounts.length === 0 || accounts.length > 0 && activeAccount !== accounts[0])) {
         console.log("Already connected")
         await loadActiveAccount()
     }
 }
 
-const loadActiveAccount = async ()=>{
+const loadActiveAccount = async () => {
     const activeAccounts = await ethereum.request({method: 'eth_accounts'})
     console.log("Active accounts", activeAccounts)
-    if(activeAccounts.length>0){
+    if (activeAccounts.length > 0) {
         activeAccount = activeAccounts[0];
         activeAccountId.innerText = `WalletID: ${activeAccount}`;
         connectButton.innerText = 'Wallet Connected';
-    }else{
+    } else {
         initializeOnboarding()
     }
 }
@@ -49,6 +50,26 @@ const initializeOnboarding = () => {
     }
 };
 
+const sendTransaction = () => {
+    console.log("Send transaction clicked")
+    const value = ethers.utils.parseEther("0.11")
+    const newValue = "0x" + value.toString();
+
+    console.log({value, newValue})
+
+    const params = [
+        {
+            from: activeAccount,
+            to: '0xd46e8dd67c5d32be8058bb8eb970870f07244567',
+            value: value.toHexString(),
+        }
+    ];
+    return ethereum.request({
+        method: 'eth_sendTransaction',
+        params,
+    })
+}
+
 if (MetaMaskOnboarding.isMetaMaskInstalled()) {
     window.ethereum.on('accountsChanged', async (newAccounts) => {
         accounts = newAccounts;
@@ -57,4 +78,4 @@ if (MetaMaskOnboarding.isMetaMaskInstalled()) {
     });
 }
 
-window.addEventListener('DOMContentLoaded',initialize)
+window.addEventListener('DOMContentLoaded', initialize)

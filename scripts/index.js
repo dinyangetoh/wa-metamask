4ed4830de8ac5706f5d453ab2a44909670c70243
David Inyangetoh <dinyangetoh@gmail.com>
2022-12-07 14:43:44 +0100

    update WA API support

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,6 @@ let activeAccount;
 const initialize = async () => {
     initializeOnboarding();
     if (ethereum.isConnected && (accounts.length === 0 || accounts.length > 0 && activeAccount !== accounts[0])) {
-        console.log("Already connected")
         await loadActiveAccount()
     }
     await initializeWorkAdventureApi()
@@ -19,7 +18,6 @@ const initialize = async () => {
 
 const loadActiveAccount = async () => {
     const activeAccounts = await ethereum.request({method: 'eth_accounts'})
-    console.log("Active accounts", activeAccounts)
     if (activeAccounts.length > 0) {
         activeAccount = activeAccounts[0];
         activeAccountId.innerText = `WalletID: ${activeAccount}`;
@@ -31,8 +29,6 @@ const loadActiveAccount = async () => {
 }
 
 const initializeOnboarding = () => {
-    console.log("initializing onboarding")
-    console.log("initializing onboarding")
     const onboarding = new MetaMaskOnboarding();
     if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
         connectButton.innerText = 'Click here to install MetaMask!';
@@ -47,7 +43,6 @@ const initializeOnboarding = () => {
         onboarding.stopOnboarding();
     } else {
         connectButton.innerText = 'Connect Wallet';
-        console.log("Accounts", accounts)
         connectButton.onclick = async () => {
             await window.ethereum.request({
                 method: 'eth_requestAccounts',
@@ -59,22 +54,26 @@ const initializeOnboarding = () => {
 const initializeWorkAdventureApi = async () => {
     console.log("Initializing WA")
     try {
-        console.log("loading WA")
-        await WA.onInit();
-        console.log("Work Adventure API loaded")
-        WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+        await new Promise((resolve, reject) => {
+            WA.onInit().then(resolve, reject)
+            setTimeout(reject, 5000)
+        })
+        listenToMessages()
+        console.log("WA Loaded")
     } catch (err) {
-        console.error("Error loading WA API", err)
-        throw err
+        console.log("Cannot load Work Adventure API")
     }
+}
+
+const listenToMessages = () => {
+    WA.chat.onChatMessage((message => {
+        console.log('New Message: ', message);
+    }));
 }
 
 const sendTransaction = () => {
     console.log("Send transaction clicked")
     const value = ethers.utils.parseEther("0.11")
-    const newValue = "0x" + value.toString();
-
-    console.log({value, newValue})
 
     const params = [
         {
@@ -97,4 +96,4 @@ if (MetaMaskOnboarding.isMetaMaskInstalled()) {
     });
 }
 
-window.addEventListener('DOMContentLoaded', initialize)
+window.addEventListener('load', initialize)
